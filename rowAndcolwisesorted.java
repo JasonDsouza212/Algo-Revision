3388c0670c4f8c7e03bebc7ed22a0e971f1851e7
Jason <jasondsouza212@gmail.com>
2022-07-14 09:19:45 +0530

    binary search algo for matrix is added

---
+++ rowAndcolwisesorted.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class rowAndcolwisesorted {
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.toString(binsearch(arr,9)));
+    }
+    static int[] binsearch(int[][] arr,int target){
+        int row=0;
+        int col=arr[0].length-1;
+        while(row<=arr.length&&col>=0){
+//            int mid=row+(col-row)/2;
+            if(arr[row][col]==target){
+                return new int[]{row,col};
+            }
+            else if(arr[row][col]<target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+
+    }
+}
