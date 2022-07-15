d5d30af581a053fbbb92bff3f1a433a5d9c302b2
Jason <jasondsouza212@gmail.com>
2022-07-15 14:46:21 +0530

    some bit operators probs are added

--- bitoperator.java
+++ bitoperator.java
@@ -1,5 +1,17 @@
 public class bitoperator {
     public static void main(String[] args) {
-        
+        int arr[]={2,3,1,2,3,6,1};
+        System.out.println(nonrepeated(arr));
+        System.out.println(oddoreven(20));
+    }
+    static boolean oddoreven(int num){
+        return ((num&1)==1);
+    }
+    static int nonrepeated(int arr[]){
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            ans=ans^arr[i];
+        }
+        return ans;
     }
 }
