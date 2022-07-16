5f019d5cee6bbdbc4c040c88f122edb6daa1f56a
Jason <jasondsouza212@gmail.com>
2022-07-16 08:57:19 +0530

    Prime number problem is solved'

--- isPrime.java
+++ isPrime.java
@@ -1,6 +1,15 @@
 public class isPrime {
     public static void main(String[] args) {
         System.out.println(orime(12));
+        int n=40;
+        boolean arr[]=new boolean[n+1];
+        primenumRange(n,arr);
+        for(int i=2;i< arr.length;i++){
+            if(!arr[i]){
+                System.out.print(i+" ");
+            }
+        }
+
     }
     static boolean orime(int num){
         if(num<=0){
@@ -15,5 +24,14 @@ public class isPrime {
             c++;
         }
         return true;
+    }
+    static void primenumRange(int n,boolean arr[]){
+        for(int i=2;i*i<n;i++){
+            if(!arr[i]){
+                for(int j=i+i;j<arr.length;j+=i){
+                    arr[j]=true;
+                }
+            }
+        }
     }
 }
